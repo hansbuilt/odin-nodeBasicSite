fc88bcc1a6615ba95b4897ab52d19eabe03bd9a3
CH8091743875984 <135175936+CH8091743875984@users.noreply.github.com>
2025-04-24 21:22:30 -0500

    Added url page connectivity

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 var http = require("http");
+var url = require("url");
+var fs = require("fs");
 
 http
   .createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("Hello World!");
+    var q = url.parse(req.url, true);
+    var filename = "." + q.pathname;
+    fs.readFile(filename + ".html", function (err, data) {
+      if (err) {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        return res.end("404 Not Found");
+      }
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      return res.end();
+    });
   })
   .listen(8080);
+
+//http://localhost:8080/
+//http://localhost:8080/about
+//http://localhost:8080/contact-me
+//http://localhost:8080/404
+//http://localhost:8080/index
