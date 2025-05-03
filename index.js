db459682d0aa1e7cc27d3e068d437b7432155371
CH8091743875984 <135175936+CH8091743875984@users.noreply.github.com>
2025-05-03 18:47:42 -0500

    Updated to make plain 8080 open to the homepage

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ var fs = require("fs");
 http
   .createServer(function (req, res) {
     var q = url.parse(req.url, true);
-    var filename = "." + q.pathname + ".html";
+    var pathname = q.pathname === "/" ? "/index" : q.pathname;
+    var filename = "." + pathname + ".html";
     fs.readFile(filename, function (err, data) {
       if (err) {
         fs.readFile("./404.html", function (err404, data404) {
